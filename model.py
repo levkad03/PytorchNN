3a47678324f0369dc44e99592eec67302ca351ad
Lev Kadatskyi <levkadatscky@gmail.com>
2024-07-03 11:19:41 +0300

    removed redundant code

--- model.py
+++ model.py
@@ -23,72 +23,12 @@ class ConvNet(nn.Module):
             nn.Dropout(),
             nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes)
+            nn.Linear(4096, num_classes),
         )
 
-        # self.layer1 = nn.Sequential(
-        #     nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
-        #     nn.ReLU(),
-        #     nn.MaxPool2d(kernel_size=3, stride=2)
-        # )
-        # self.layer2 = nn.Sequential(
-        #     nn.Conv2d(64, 192, kernel_size=5, padding=2),
-        #     nn.ReLU(),
-        #     nn.MaxPool2d(kernel_size=3, stride=2)
-        # )
-        # self.layer3 = nn.Sequential(
-        #     nn.Conv2d(192, 384, kernel_size=3, padding=1),
-        #     nn.ReLU(),
-        # )
-        # self.layer4 = nn.Sequential(
-        #     nn.Conv2d(384, 256, kernel_size=3, padding=1),
-        #     nn.ReLU(),
-        # )
-        # self.layer5 = nn.Sequential(
-        #     nn.Conv2d(256, 256, kernel_size=3, padding=1),
-        #     nn.ReLU(),
-        #     nn.MaxPool2d(kernel_size=3, stride=2)
-        # )
-        # self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
-        # self.fc = nn.Sequential(
-        #     nn.Dropout(0.5),
-        #     nn.Linear(256*6*6, 4096),
-        #     nn.ReLU()
-        # )
-        # self.fc1 = nn.Sequential(
-        #     nn.Dropout(0.5),
-        #     nn.Linear(4096, 4096),
-        #     nn.ReLU()
-        # )
-        # self.fc2 = nn.Sequential(
-        #     nn.Linear(4096, num_classes),
-        # )
-
     def forward(self, x):
-
         out = self.pretrained_features(x)
         out = out.view(out.size(0), -1)
         out = self.classifier(out)
 
-        # out = self.layer1(x)
-        # out = self.layer2(out)
-        # out = self.layer3(out)
-        # out = self.layer4(out)
-        # out = self.layer5(out)
-        # out = self.avgpool(out)
-        # out = torch.flatten(out, 1)
-        # out = self.fc(out)
-        # out = self.fc1(out)
-        # out = self.fc2(out)
         return out
-
-        # x = self.pool(F.relu(self.conv1(x)))
-        # x = self.pool(F.relu(self.conv2(x)))
-        # x = self.dropout(x)
-        # x = self.pool(F.relu(self.conv3(x)))
-        # x = x.view(-1, 22 * 22 * 30)
-        # x = F.relu(self.fc1(x))
-        # x = F.relu(self.fc2(x))
-        # x = self.fc3(x)
-
-        # return x
